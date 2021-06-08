f69c5ea391ad928064f02f7b5eeb1142eebe2b4a
Lim Li Khia <likhia@hotmail.com>
2021-06-08 23:33:12 +0800

    updates

--- example1/src/main/java/com/myspace/example1/DMNApp.java
+++ example1/src/main/java/com/myspace/example1/DMNApp.java
@@ -32,7 +32,7 @@ import org.kie.server.client.KieServicesFactory;
 import org.kie.server.client.RuleServicesClient;
 import org.kie.api.runtime.ClassObjectFilter;
 
-
+import com.myspace.dmnexample1.EntityList;
 
 public class DMNApp {
 
@@ -40,9 +40,11 @@ public class DMNApp {
     //private static final String modelNamespace = "https://kiegroup.org/dmn/_CFAB5392-6988-42B9-82D2-137A934CA4DE";
     //private static final String modelName = "SampleDMN1";
 
-    private static final String modelNamespace = "https://kiegroup.org/dmn/_130EFB28-D8FD-495B-B01E-13B72C1F4907";
-    private static final String modelName = "SampleDMN2";
+    //private static final String modelNamespace = "https://kiegroup.org/dmn/_130EFB28-D8FD-495B-B01E-13B72C1F4907";
+    //private static final String modelName = "SampleDMN2";
 
+    private static final String modelNamespace = "https://kiegroup.org/dmn/_779852A8-1977-4863-8BC0-7D5ED2AEF291";
+    private static final String modelName = "ListDMN";
 
     public static void main(String[] args) {
         try {
@@ -50,19 +52,34 @@ public class DMNApp {
             String serverUrl = "http://localhost:8080/kie-server/services/rest/server";
             String username = "rhdmAdmin";
             String password = "redhat@123";
+
+            // Define KIE services configuration and client:
+            Set<Class<?>> allClasses = new HashSet<Class<?>>();
+            allClasses.add(EntityList.class);
+           
             KieServicesConfiguration config = KieServicesFactory.newRestConfiguration(serverUrl, username, password);
             config.setMarshallingFormat(MarshallingFormat.JAXB);
+            config.addExtraClasses(allClasses);
             
             //https://access.redhat.com/documentation/en-us/red_hat_decision_manager/7.1/html-single/designing_a_decision_service_using_dmn_models/index
             KieServicesClient kieServicesClient = KieServicesFactory.newKieServicesClient(config);
          
             DMNServicesClient dmnClient = kieServicesClient.getServicesClient(DMNServicesClient.class );
          
-            DMNContext dmnContext = dmnClient.newContext(); 
-            dmnContext.set("category", "Cat1");
-            dmnContext.set("costprice", Double.valueOf(10.00));
-            dmnContext.set("quantity", Integer.valueOf(1));
+            EntityList entityList = new EntityList();
+            List<String> nameList = new ArrayList();
+            nameList.add("IPP");
+            nameList.add("TEST");
 
+            entityList.setName(nameList);
+
+            DMNContext dmnContext = dmnClient.newContext(); 
+            //dmnContext.set("category", "Cat1");
+            //dmnContext.set("costprice", Double.valueOf(10.00));
+            //dmnContext.set("quantity", Integer.valueOf(1))
+            
+            dmnContext.set("entityList", entityList );
+            
            
             ServiceResponse<DMNResult> serverResp =   dmnClient.evaluateAll(containerName, modelNamespace, modelName, dmnContext);
 
